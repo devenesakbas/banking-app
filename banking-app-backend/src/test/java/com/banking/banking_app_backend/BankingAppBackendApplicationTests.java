9edd53cf4b7a335b2017a5b509247a5ea9be74eb
devenesakbas <dev.enes.akbas@gmail.com>
2026-08-06 11:23:19 +0300

    chore: initial project structure with frontend and backend

---
+++ banking-app-backend/src/test/java/com/banking/banking_app_backend/BankingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.banking_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ banking-app-fronted/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 style={{ textAlign: 'center' }}>Banking App Başlıyor..</h1>
+    </>
+  )
+}
+
+export default App
